6f1f88880965086133f5660f51d061db39505dfc
Sayam Ganguly <Sayam Ganguly>
2016-10-14 20:39:59 -0400

    adding logs

--- app/src/main/java/edu/ohio_state/cse/nagger/SplashActivity.java
+++ app/src/main/java/edu/ohio_state/cse/nagger/SplashActivity.java
@@ -3,6 +3,7 @@ package edu.ohio_state.cse.nagger;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 
 public class SplashActivity extends AppCompatActivity {
@@ -11,12 +12,13 @@ public class SplashActivity extends AppCompatActivity {
     private int mElapsedTime = 0;
     private int mTimeStep = 100;
     private boolean isTouched = false;
+    private final String TAG = getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
+        Log.d(TAG,"Inside Method OnCreate");
         Thread splashThread = new Thread(){
             public void run(){
                 try {
@@ -43,5 +45,35 @@ public class SplashActivity extends AppCompatActivity {
             isTouched = true;
         }
         return true;
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.d(TAG,"Inside Method OnStart");
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(TAG,"Inside Method OnResume");
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.d(TAG,"Inside Method OnPause");
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(TAG,"Inside Method OnStop");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG,"Inside Method OnDestroy");
     }
 }
